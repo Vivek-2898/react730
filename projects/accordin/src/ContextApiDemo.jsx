6c112e346f7a571a0bdfcf9173abf7da4efec714
Vivek <vivekayyavar6@gmail.com>
2024-03-24 14:51:33 +0530

    implement ContextApiDemo

---
+++ projects/accordin/src/ContextApiDemo.jsx
@@ -0,0 +1,18 @@
+import React, { useState,useContext } from 'react'
+import Component from './Component1'
+import { mainContext } from './main'
+
+const ContextApiDemo = () =>
+{
+  const [count, setCount] = useState(0)
+  
+  return (
+    <div style={{width:'400px',height:'400px',border:'1px solid red'}}>
+      <button onClick={()=>setCount(count+1)}>incr</button>
+      <mainContext.Provider value={count}>
+        <Component />
+			</mainContext.Provider>
+		</div>
+	)
+}
+export default ContextApiDemo
--- projects/accordin/src/main.jsx
+++ projects/accordin/src/main.jsx
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createContext } from 'react' // Import createContext separately
 import App from './App.jsx'
 import './index.css'
 import './Todo.css'
 import TodoApp from './TodoApp.jsx'
 import Todo1 from './Todo1.jsx'
+import ContextApiDemo from './ContextApiDemo.jsx'
 
+export const mainContext = createContext()
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     {/* <App /> */}
     {/* <TodoApp/> */}
-    <Todo1/>
-  </React.StrictMode>,
+     {/* <Todo1/> */}
+     <ContextApiDemo/>
+  </React.StrictMode>
 )
