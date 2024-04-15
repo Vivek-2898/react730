bfcb03ff3e1e689af57a5b4f7126949bef1e4a05
Vivek <vivekayyavar6@gmail.com>
2024-04-15 15:19:00 +0530

    implement Hooks

--- projects/crud/src/main.jsx
+++ projects/crud/src/main.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Hooks from './Hooks.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Hooks/>
   </React.StrictMode>,
 )
